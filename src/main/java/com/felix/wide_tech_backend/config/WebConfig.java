b8f9309a6ff0da5ce7685df9aefd3421f511f643
lukaslinardi <lukaslinardi@gmail.com>
2025-02-15 21:10:58 +0700

    fix: add bean annotation

--- src/main/java/com/felix/wide_tech_backend/config/WebConfig.java
+++ src/main/java/com/felix/wide_tech_backend/config/WebConfig.java
@@ -1,11 +1,13 @@
 package com.felix.wide_tech_backend.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig {
+    @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
